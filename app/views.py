c1fd6479a7dfd06b7b304c370597ea54ff1fe082
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2023-04-29 15:32:27 -0400

    A new begin

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Producto
+
+# Create your views here.
+
+def home(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos' : productos
+    }
+    return render(request, 'app/home.html', data)
+
+def contacto(request):
+    return render(request, 'app/contacto.html')
+
+def galeria(request):
+    return render(request, 'app/galeria.html')
